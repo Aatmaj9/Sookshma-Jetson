af82eda1ea6ad867739fc1a533a8d8d24fece98f
Aatmaj9 <na22b018@smail.iitm.ac.in>
2025-10-07 19:14:08 +0530

    new image

--- build.sh
+++ build.sh
@@ -2,6 +2,6 @@ docker buildx build \
   --platform linux/arm64 \
   --pull \
   -f .devcontainer/Dockerfile \
-  -t aatmaj9/sookshma-jetson:2.0 \
+  -t aatmaj9/sookshma-jetson:3.0 \
   --push \
   .
