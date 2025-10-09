4ab0b537f5f9d8085cdac633007bb435bb3345ba
Aatmaj9 <na22b018@smail.iitm.ac.in>
2025-10-09 17:26:57 +0530

    docker file major change

--- build.sh
+++ build.sh
@@ -2,6 +2,6 @@ docker buildx build \
   --platform linux/arm64 \
   --pull \
   -f .devcontainer/Dockerfile \
-  -t aatmaj9/sookshma-jetson:4.0 \
+  -t aatmaj9/sookshma-jetson:5.0 \
   --push \
   .
