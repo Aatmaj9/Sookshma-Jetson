a76dfe9dc69846d7b49d828a244c352135ddd010
Aatmaj9 <na22b018@smail.iitm.ac.in>
2025-10-09 20:15:16 +0530

    zed works

--- zed.sh
+++ zed.sh
@@ -1,5 +1,4 @@
 ros2 launch zed_wrapper zed_camera.launch.py camera_model:=zed2i &
 sleep 3
 
-# Start the Foxglove WebSocket bridge on port 8765 (default)
 ros2 run foxglove_bridge foxglove_bridge --port 8765
