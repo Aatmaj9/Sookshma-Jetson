7731838d089fdfd614efa245ef65f730b0e53546
Aatmaj9 <na22b018@smail.iitm.ac.in>
2025-10-10 18:54:51 +0530

    serial rules updated

--- upload.sh
+++ upload.sh
@@ -5,7 +5,7 @@ set -e
 SKETCH_PATH="./arduino/sookshma_masv01"
 
 if [ "$(uname -m)" = "x86_64" ]; then
-    arduino-cli upload -p /dev/arduino --fqbn arduino:sam:arduino_due_x_dbg "$SKETCH_PATH"
+    arduino-cli upload -p /dev/ttyACM0 --fqbn arduino:sam:arduino_due_x_dbg "$SKETCH_PATH"
 else
-    arduino-cli upload -p /dev/arduino --fqbn per1234:sam:arduino_due_x_dbg "$SKETCH_PATH"
+    arduino-cli upload -p /dev/ttyACM0 --fqbn per1234:sam:arduino_due_x_dbg "$SKETCH_PATH"
 fi
